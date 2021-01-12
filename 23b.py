08435d8ac68e9934c08979783550e2e80cfd584c
nightjuggler <pius@nightjuggler.com>
2021-01-12 07:41:18 -0800

    Cleaned up

--- 23b.py
+++ 23b.py
@@ -1,22 +1,13 @@
 import sys
 
 class Elem(object):
-	def __init__(self, value):
+	def __init__(self, value, prev):
 		self.value = value
-
-class LinkedList(object):
-	def __init__(self):
-		self.tail = None
-
-	def append(self, value):
-		tail = self.tail
-		self.tail = elem = Elem(value)
-		if tail is None:
-			elem.next = elem
+		if prev:
+			self.next = prev.next
+			prev.next = self
 		else:
-			elem.next = tail.next
-			tail.next = elem
-		return elem
+			self.next = self
 
 def llstr(head, sep=' '):
 	values = []
@@ -29,59 +20,71 @@ def llstr(head, sep=' '):
 	return sep.join([str(value) for value in values])
 
 def main(args):
-	input_lookup = {
-		'default': '463528179',
-		'example': '389125467',
-	}
-	input_string = input_lookup.get(args[0], args[0]) if args else input_lookup['default']
+	part_one = False
+	num_cups = 1000000
+	num_moves = 10000000
+	input_string = '463528179'
+
+	for arg in args:
+		if arg == '-1':
+			part_one = True
+			num_cups = 9
+			num_moves = 100
+		elif arg == '-x':
+			input_string = '389125467'
 
 	s = {}
-	cups = LinkedList()
+	h = None
 	for c in input_string:
 		c = ord(c) - 48
 		assert 1 <= c <= 9
 		assert c not in s
-		s[c] = cups.append(c)
+		s[c] = h = Elem(c, h)
 	assert len(s) == 9
+
+	d = s[1]
 	for c in range(2, 10):
-		s[c].dest = s[c - 1]
-	d = s[9]
-	for c in range(10, 1000001):
-		e = cups.append(c)
+		e = s[c]
 		e.dest = d
 		d = e
+	for c in range(10, num_cups + 1):
+		h = Elem(c, h)
+		h.dest = d
+		d = h
 	s[1].dest = d
 
-	h = cups.tail.next
-#	for move in range(1, 101):
-	for move in range(10000000):
-#		print('move {}:'.format(move), llstr(h))
+	h = h.next
+	for move in range(1, num_moves + 1):
+		if part_one:
+			print('move {}:'.format(move), llstr(h))
 
-		d = h.dest
 		x = h.next
 		y = x.next
 		z = y.next
-		h.next = z.next
-		h = h.next
 
+		d = h.dest
 		while d is x or d is y or d is z:
 			d = d.dest
 
+		h.next = z.next
 		z.next = d.next
 		d.next = x
+		h = h.next
 
-#	print('final:', llstr(h))
+	if part_one:
+		print('final:', llstr(h))
 
 	while h.value != 1:
 		h = h.next
 
-#	print(llstr(h, sep='')[1:])
-
-	x = h.next
-	y = x.next
-	x = x.value
-	y = y.value
-	print(x, '*', y, '=', x * y)
+	if part_one:
+		print(llstr(h, sep='')[1:])
+	else:
+		x = h.next
+		y = x.next
+		x = x.value
+		y = y.value
+		print(x, '*', y, '=', x * y)
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
