b2580e3ede4c6d67ef959d3d952523b5e3a4e88c
nightjuggler <pius@nightjuggler.com>
2021-01-13 01:17:18 -0800

    Simplify

--- 17.py
+++ 17.py
@@ -6,12 +6,6 @@ def err(message, *args):
 class State(object):
 	def __init__(self):
 		self.active = set()
-		self.min_x = None
-		self.max_x = None
-		self.min_y = None
-		self.max_y = None
-		self.min_z = None
-		self.max_z = None
 
 	def add(self, x, y, z):
 		if self.active:
@@ -22,47 +16,48 @@ class State(object):
 			if   z < self.min_z: self.min_z = z
 			elif z > self.max_z: self.max_z = z
 		else:
-			self.min_x = x
-			self.max_x = x
-			self.min_y = y
-			self.max_y = y
-			self.min_z = z
-			self.max_z = z
+			self.min_x = self.max_x = x
+			self.min_y = self.max_y = y
+			self.min_z = self.max_z = z
 
 		self.active.add((x, y, z))
 
 	def print(self):
 		active = self.active
-		for z in range(self.min_z, self.max_z + 1):
+		if not active:
+			return
+
+		zlo, zhi = self.min_z, self.max_z + 1
+		ylo, yhi = self.min_y, self.max_y + 1
+		xlo, xhi = self.min_x, self.max_x + 1
+
+		for z in range(zlo, zhi):
 			print('z={}'.format(z))
-			for y in range(self.min_y, self.max_y + 1):
+			for y in range(ylo, yhi):
 				print(''.join(['#' if (x, y, z) in active else '.'
-					for x in range(self.min_x, self.max_x + 1)]))
+					for x in range(xlo, xhi)]))
 			print()
 
 	def play(self):
-		new_state = State()
-
-		zlo = self.min_z - 1
-		zhi = self.max_z + 2
-		ylo = self.min_y - 1
-		yhi = self.max_y + 2
-		xlo = self.min_x - 1
-		xhi = self.max_x + 2
 		active = self.active
+		if not active:
+			return self
+
+		zlo, zhi = self.min_z - 1, self.max_z + 2
+		ylo, yhi = self.min_y - 1, self.max_y + 2
+		xlo, xhi = self.min_x - 1, self.max_x + 2
+
+		new_state = State()
 
 		for z in range(zlo, zhi):
 			for y in range(ylo, yhi):
 				for x in range(xlo, xhi):
-					alive = (x, y, z) in active
+					# Note that the current cell is included in this neighbor count
 					n = sum([(i, j, k) in active
 						for k in range(z - 1, z + 2)
 						for j in range(y - 1, y + 2)
-						for i in range(x - 1, x + 2)]) - alive
-					if alive:
-						if n == 2 or n == 3:
-							new_state.add(x, y, z)
-					elif n == 3:
+						for i in range(x - 1, x + 2)])
+					if n == 3 or n == 4 and (x, y, z) in active:
 						new_state.add(x, y, z)
 		return new_state
 
--- 17b.py
+++ 17b.py
@@ -6,14 +6,6 @@ def err(message, *args):
 class State(object):
 	def __init__(self):
 		self.active = set()
-		self.min_x = None
-		self.max_x = None
-		self.min_y = None
-		self.max_y = None
-		self.min_z = None
-		self.max_z = None
-		self.min_w = None
-		self.max_w = None
 
 	def add(self, x, y, z, w):
 		if self.active:
@@ -26,23 +18,23 @@ class State(object):
 			if   w < self.min_w: self.min_w = w
 			elif w > self.max_w: self.max_w = w
 		else:
-			self.min_x = x
-			self.max_x = x
-			self.min_y = y
-			self.max_y = y
-			self.min_z = z
-			self.max_z = z
-			self.min_w = w
-			self.max_w = w
+			self.min_x = self.max_x = x
+			self.min_y = self.max_y = y
+			self.min_z = self.max_z = z
+			self.min_w = self.max_w = w
 
 		self.active.add((x, y, z, w))
 
 	def print(self):
+		active = self.active
+		if not active:
+			return
+
 		wlo, whi = self.min_w, self.max_w + 1
 		zlo, zhi = self.min_z, self.max_z + 1
 		ylo, yhi = self.min_y, self.max_y + 1
 		xlo, xhi = self.min_x, self.max_x + 1
-		active = self.active
+
 		for w in range(wlo, whi):
 			for z in range(zlo, zhi):
 				print('z={}, w={}'.format(z, w))
@@ -52,31 +44,28 @@ class State(object):
 				print()
 
 	def play(self):
-		new_state = State()
-
-		wlo = self.min_w - 1
-		whi = self.max_w + 2
-		zlo = self.min_z - 1
-		zhi = self.max_z + 2
-		ylo = self.min_y - 1
-		yhi = self.max_y + 2
-		xlo = self.min_x - 1
-		xhi = self.max_x + 2
 		active = self.active
+		if not active:
+			return self
+
+		wlo, whi = self.min_w - 1, self.max_w + 2
+		zlo, zhi = self.min_z - 1, self.max_z + 2
+		ylo, yhi = self.min_y - 1, self.max_y + 2
+		xlo, xhi = self.min_x - 1, self.max_x + 2
+
+		new_state = State()
 
 		for w in range(wlo, whi):
 			for z in range(zlo, zhi):
 				for y in range(ylo, yhi):
 					for x in range(xlo, xhi):
+						# Note that the current cell is included in this neighbor count
 						n = sum([(i, j, k, m) in active
 							for m in range(w - 1, w + 2)
 							for k in range(z - 1, z + 2)
 							for j in range(y - 1, y + 2)
 							for i in range(x - 1, x + 2)])
-						if (x, y, z, w) in active:
-							if n == 3 or n == 4:
-								new_state.add(x, y, z, w)
-						elif n == 3:
+						if n == 3 or n == 4 and (x, y, z, w) in active:
 							new_state.add(x, y, z, w)
 		return new_state
 
