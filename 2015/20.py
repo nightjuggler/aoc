ecac70373adcf4e487c13cd092b16b2d7672229a
nightjuggler <pius@nightjuggler.com>
2021-02-08 19:32:20 -0800

    Sacrifice performance for clarity

--- 2015/20.py
+++ 2015/20.py
@@ -1,28 +1,29 @@
 def solve(n, size):
-	if n < 4:
-		print(1 if n == 0 else 2 if n <= 2 else 3)
+	if n == 1:
+		print(1)
 		return
 	if size == 0:
-		size = (n // 2 if n >= 1000 else n) + 1
+		size = n + 2
 		print('Setting the array size to', size)
 	s = [0] * size
-	for elf in range(2, size // 2):
+	for elf in range(1, size // 2):
 		for i in range(elf, size, elf):
 			s[i] += elf
 		i = elf * 2
+		if s[i] + i >= n:
+			print('House', i, 'receives', 10 * (s[i] + i), 'presents')
+			return
+		i += 1
 		if s[i] + i >= n:
 			print(i)
 			return
 	print('Please try an array size >', size)
 
 def solve_naive(n):
-	if n == 0:
-		print(1)
-		return
-	x = 2
+	x = 1
 	while True:
-		s = x
-		for d in range(2, int(x ** 0.5) + 1):
+		s = 0
+		for d in range(1, int(x ** 0.5) + 1):
 			f, r = divmod(x, d)
 			if r == 0:
 				s += d if d == f else d + f
@@ -44,9 +45,8 @@ def main():
 		print('The number of presents must be > 0!')
 		return
 	n, r = divmod(n, 10)
-	if r == 0:
-		# Every house gets at least 10 presents from the first elf.
-		n -= 1
+	if r:
+		n += 1
 
 	size = args.size
 	if size < 0:
