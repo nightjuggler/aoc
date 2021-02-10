ec5399d12f1ad0497fac418ac12ea40a9f1d42f0
nightjuggler <pius@nightjuggler.com>
2021-02-10 00:31:35 -0800

    Add 2018 day 3

---
+++ 2018/03.py
@@ -0,0 +1,38 @@
+import re
+import sys
+
+def err(message, *args):
+	sys.exit(message.format(*args) if args else message)
+
+def main():
+	claims = []
+	max_x = max_y = 0
+	line_pattern = re.compile('^#{n} @ ({n}|0),({n}|0): ({n})x({n})$'.format(n='[1-9][0-9]*'))
+
+	for i, line in enumerate(sys.stdin, start=1):
+		m = line_pattern.match(line)
+		if not m:
+			err('Line {} doesn\'t match pattern!', i)
+		x, y, w, h = map(int, m.groups())
+		x2 = x + w
+		y2 = y + h
+		if x2 > max_x: max_x = x2
+		if y2 > max_y: max_y = y2
+		claims.append((x, y, x2, y2))
+
+	print(max_y, 'x', max_x)
+	fabric = [[0] * max_x for i in range(max_y)]
+
+	for x1, y1, x2, y2 in claims:
+		for y in range(y1, y2):
+			for x in range(x1, x2):
+				fabric[y][x] += 1
+	overlap = 0
+	for y in range(max_y):
+		for x in range(max_x):
+			if fabric[y][x] > 1:
+				overlap += 1
+	print(overlap)
+
+if __name__ == '__main__':
+	main()
