50b30b51b98b1f691687d5f5ccd1c7542c622583
nightjuggler <pius@nightjuggler.com>
2021-12-03 02:22:22 -0800

    Different implementation of 2021 day 3 part 2

--- 2021/03.py
+++ 2021/03.py
@@ -35,15 +35,30 @@ def get_rating(keep, d1, d2):
 		pos += 1
 	return int(keep[0], 2)
 
+def get_rating2(keep, co2):
+	pos = 0
+	while len(keep) > 1:
+		zeros, ones = [], []
+		for n in keep:
+			(zeros if n[pos] == '0' else ones).append(n)
+		keep_ones = len(ones) >= len(zeros)
+		if co2: keep_ones = not keep_ones
+		keep = ones if keep_ones else zeros
+		pos += 1
+	return int(keep[0], 2)
+
 def part2():
 	numbers = [line.rstrip() for line in sys.stdin]
 
-	oxygen_rating = get_rating(numbers, '1', '0')
-	co2_rating = get_rating(numbers, '0', '1')
+	oxygen = get_rating(numbers, '1', '0')
+	co2 = get_rating(numbers, '0', '1')
 
-	print('oxygen =', oxygen_rating)
-	print('CO2 =', co2_rating)
-	print('oxygen * CO2 =', oxygen_rating * co2_rating)
+	print(f'oxygen = {oxygen}, CO2 = {co2}, oxygen * CO2 = {oxygen * co2}')
+
+	oxygen = get_rating2(numbers, False)
+	co2 = get_rating2(numbers, True)
+
+	print(f'oxygen = {oxygen}, CO2 = {co2}, oxygen * CO2 = {oxygen * co2}')
 
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
