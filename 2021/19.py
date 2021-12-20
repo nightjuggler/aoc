3094eef7aa3c1a6d48b0f5a936c9ee948008a5a1
nightjuggler <pius@nightjuggler.com>
2021-12-20 01:16:41 -0800

    Cleaned up

--- 2021/19.py
+++ 2021/19.py
@@ -35,6 +35,7 @@ def multiply_matrices(m1, m2):
 	return tuple([tuple([sum([e1 * e2 for e1, e2 in zip(row, col)]) for col in m2]) for row in m1])
 
 def get_rotations():
+	# cos(t), sin(t) for t in 0, 90, 180, and 270 degrees
 	cos_sin = ((1, 0), (0, 1), (-1, 0), (0, -1))
 	rotations = set()
 	for zcos, zsin in cos_sin:
@@ -72,13 +73,45 @@ def find_overlap(rotations, scanners, s1, s2):
 			for b1, b1_set in beacons1:
 				x_set = b1_set & rotated_set
 				if len(x_set) >= 11:
-					# b1 and b2 are the same beacon
+					# b1 and b2 are the same beacon.
+					# If p is the position of s2 relative to s1,
+					# then since b2 is relative to s2, p + b2 = b1.
+					# Thus p = b1 - b2
 					x2, y2, z2 = rotate_point(rot, b2)
 					x1, y1, z1 = b1
 					p = (x1 - x2, y1 - y2, z1 - z2)
 					print(f'Scanner {s2} is at {p} relative to scanner {s1}')
 					return p, rot
 	return None
+
+def path_to_scanner0(i, overlap_map):
+	tried = [False] * len(overlap_map)
+	tried[i] = True
+	q = deque()
+	for j, overlap in overlap_map[i]:
+		q.append((j, [overlap]))
+	while q:
+		j, path = q.popleft()
+		if tried[j]:
+			continue
+		tried[j] = True
+		if j == 0:
+			return path
+		for j, overlap in overlap_map[j]:
+			new_path = path.copy()
+			new_path.append(overlap)
+			q.append((j, new_path))
+
+	print(f'Cannot find a way to map scanner {i} to scanner 0!')
+	return None
+
+def follow_path(p1, path):
+	for p2, rot2 in path:
+		p1 = rotate_point(rot2, p1)
+		x1, y1, z1 = p1
+		x2, y2, z2 = p2
+		p1 = (x1 + x2, y1 + y2, z1 + z2)
+	return p1
 
 def main():
 	scanners = read_input()
@@ -92,52 +125,24 @@ def main():
 	num_scanners = len(scanners)
 	for i in range(num_scanners-1):
 		for j in range(i+1, num_scanners):
-			overlap_ji = find_overlap(rotations, scanners, i, j)
-			if overlap_ji:
-				overlap_map[j].append((i, overlap_ji))
-				overlap_ij = find_overlap(rotations, scanners, j, i)
-				overlap_map[i].append((j, overlap_ij))
+			overlap = find_overlap(rotations, scanners, i, j)
+			if overlap:
+				overlap_map[j].append((i, overlap))
+				overlap = find_overlap(rotations, scanners, j, i)
+				overlap_map[i].append((j, overlap))
 
 	beacon_positions = set([b for b, b_set in scanners[0]])
 	scanner_positions = [(0, 0, 0)]
 
 	for i in range(1, num_scanners):
-		tried = [False] * num_scanners
-		tried[i] = True
-		success = False
-		q = deque()
-		for j, p_rot in overlap_map[i]:
-			q.append((j, [p_rot]))
-		while q:
-			j, path = q.popleft()
-			if tried[j]:
-				continue
-			tried[j] = True
-			if j == 0:
-				success = True
-				break
-			for j, p_rot in overlap_map[j]:
-				new_path = path.copy()
-				new_path.append(p_rot)
-				q.append((j, new_path))
-		if not success:
-			print(f'Cannot find a way to map scanner {i} to scanner 0!')
+		path = path_to_scanner0(i, overlap_map)
+		if not path:
 			return
-		p1, rot1 = path[0]
-		for p2, rot2 in path[1:]:
-			p1 = rotate_point(rot2, p1)
-			x1, y1, z1 = p1
-			x2, y2, z2 = p2
-			p1 = (x1 + x2, y1 + y2, z1 + z2)
+		p1 = follow_path(path[0][0], path[1:])
 		print(f'Scanner {i} is at {p1} relative to scanner 0')
 		scanner_positions.append(p1)
 		for p1, b_set in scanners[i]:
-			for p2, rot2 in path:
-				p1 = rotate_point(rot2, p1)
-				x1, y1, z1 = p1
-				x2, y2, z2 = p2
-				p1 = (x1 + x2, y1 + y2, z1 + z2)
-			beacon_positions.add(p1)
+			beacon_positions.add(follow_path(p1, path))
 	print('Part 1:', len(beacon_positions))
 
 	max_distance = 0
