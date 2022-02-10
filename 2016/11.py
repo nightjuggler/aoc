c89f167c7eb0e19f6393a36975218a4d3ca0efb6
nightjuggler <pius@nightjuggler.com>
2022-02-09 23:03:39 -0800

    Add 2016 day 11 It's slow for part 2 (~110 seconds!) but it finds the answer ...

---
+++ 2016/11.py
@@ -0,0 +1,160 @@
+from collections import deque
+from itertools import combinations
+import re
+import sys
+
+def read_input():
+	x = 'an? [a-z]+(?:(?:-compatible microchip)|(?: generator))'
+	pattern = re.compile('^The ([a-z]+) floor contains ((?:nothing relevant)|'
+		f'(?:{x}(?:(?:(?:, {x})+,)? and {x})?))\\.$')
+	ordinals = ('first', 'second', 'third', 'fourth')
+	max_floor = len(ordinals) - 1
+
+	all_microchips = set()
+	all_generators = set()
+	floors = []
+
+	floor = None
+	for floor, line in enumerate(sys.stdin):
+		if floor > max_floor:
+			break
+		m = pattern.match(line)
+		if not m or m.group(1) != ordinals[floor]:
+			raise SystemExit(f'Syntax error on input line {floor+1}!')
+		contains = m.group(2)
+		if contains == 'nothing relevant':
+			assert floor > 0
+			floors.append((frozenset(), frozenset()))
+			continue
+		contains = contains.split(', ')
+		if len(contains) == 1:
+			i = contains[0].find(' and a')
+			if i > 0:
+				contains = contains[0]
+				contains = [contains[:i], contains[i+5:]]
+		else:
+			assert len(contains) > 2
+			assert contains[-1].startswith('and a')
+			contains[-1] = contains[-1][4:]
+		microchips = set()
+		generators = set()
+		for item in contains:
+			a, b, c = item.split(' ')
+			if c == 'microchip':
+				b = b.removesuffix('-compatible')
+				assert b not in all_microchips
+				all_microchips.add(b)
+				microchips.add(b)
+			else:
+				assert b not in all_generators
+				all_generators.add(b)
+				generators.add(b)
+		if microchips and generators:
+			assert microchips <= generators
+		floors.append((frozenset(microchips), frozenset(generators)))
+
+	if floor != max_floor:
+		raise SystemExit(f'Expected exactly {max_floor+1} input lines!')
+
+	assert all_microchips == all_generators
+	assert len(floors) == max_floor + 1
+
+	return (0, tuple(floors))
+
+def valid(items):
+	m1, g1, m2, g2 = items
+	return (not (m1 and g1) or m1 <= g1) and (not (m2 and g2) or m2 <= g2)
+
+def two_combos(items, next_items):
+	m1, g1 = items
+	m2, g2 = next_items
+
+	for x in m1 & g1:
+		x = (x,)
+		yield (m1.difference(x), g1.difference(x), m2.union(x), g2.union(x))
+	for x in combinations(m1, 2):
+		yield (m1.difference(x), g1, m2.union(x), g2)
+	for x in combinations(g1, 2):
+		yield (m1, g1.difference(x), m2, g2.union(x))
+
+def one_combos(items, next_items):
+	m1, g1 = items
+	m2, g2 = next_items
+
+	for x in m1:
+		x = (x,)
+		yield (m1.difference(x), g1, m2.union(x), g2)
+	for x in g1:
+		x = (x,)
+		yield (m1, g1.difference(x), m2, g2.union(x))
+
+def solve(state):
+	q = deque()
+	q.append((0, state))
+	state_cache = {}
+	min_steps = None
+
+	while q:
+		steps, state = q.popleft()
+		cached_steps = state_cache.get(state)
+		if cached_steps is not None and cached_steps <= steps:
+			continue
+		state_cache[state] = steps
+		floor, floors = state
+		max_floor = len(floors) - 1
+		if not max_floor:
+			min_steps = steps
+			continue
+		if min_steps and steps >= min_steps:
+			continue
+
+		items = floors[floor]
+		floors = list(floors)
+		steps += 1
+
+		if floor < max_floor:
+			next_floor = floor + 1
+			next_items = floors[next_floor]
+			combos = list(filter(valid, two_combos(items, next_items)))
+			if not combos:
+				combos = filter(valid, one_combos(items, next_items))
+			for mics, gens, next_mics, next_gens in combos:
+				floors[floor] = (mics, gens)
+				floors[next_floor] = (next_mics, next_gens)
+				if floor == 0 and not (mics or gens):
+					q.append((steps, (0, tuple(floors[1:]))))
+				else:
+					q.append((steps, (next_floor, tuple(floors))))
+			floors[next_floor] = next_items
+
+		if floor:
+			next_floor = floor - 1
+			combos = filter(valid, one_combos(items, floors[next_floor]))
+			for mics, gens, next_mics, next_gens in combos:
+				floors[floor] = (mics, gens)
+				floors[next_floor] = (next_mics, next_gens)
+				q.append((steps, (next_floor, tuple(floors))))
+	return min_steps
+
+def add_items(state, items):
+	floor, floors = state
+	mics, gens = floors[floor]
+	floors = list(floors)
+	floors[floor] = (mics.union(items), gens.union(items))
+	return (floor, tuple(floors))
+
+def main():
+	state = read_input()
+	print('Part 1:', solve(state))
+	print('Part 2:', solve(add_items(state, ('elerium', 'dilithium'))))
+
+# > python3 11.py < data/11.example
+# Part 1: 11
+# Part 2: None
+
+# > python3 11.py < data/11.input
+# Part 1: 47
+# Part 2: 71
+
+if __name__ == '__main__':
+	main()
