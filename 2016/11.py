9edf81eb6a06936b1c5fd569b662b8c371ae7f9d
nightjuggler <pius@nightjuggler.com>
2022-02-12 17:19:56 -0800

    Move 11c.py to 11.py

--- 2016/11.py
+++ 2016/11.py
@@ -14,6 +14,15 @@ def read_input():
 	all_generators = set()
 	floors = []
 
+	elements = {}
+	next_id = 0
+	def get_id(x):
+		nonlocal next_id
+		x = elements.setdefault(x, next_id)
+		if x == next_id:
+			next_id += 1
+		return x
+
 	floor = None
 	for floor, line in enumerate(sys.stdin):
 		if floor > max_floor:
@@ -42,10 +51,12 @@ def read_input():
 			a, b, c = item.split(' ')
 			if c == 'microchip':
 				b = b.removesuffix('-compatible')
+				b = get_id(b)
 				assert b not in all_microchips
 				all_microchips.add(b)
 				microchips.add(b)
 			else:
+				b = get_id(b)
 				assert b not in all_generators
 				all_generators.add(b)
 				generators.add(b)
@@ -58,44 +69,66 @@ def read_input():
 
 	assert all_microchips == all_generators
 	assert len(floors) == max_floor + 1
-	return floors
-
-def valid(items):
-	m1, g1, m2, g2 = items
-	return (not (m1 and g1) or m1 <= g1) and (not (m2 and g2) or m2 <= g2)
+	return next_id, floors
 
 def two_combos(items, next_items):
 	m1, g1 = items
 	m2, g2 = next_items
 
 	for x in m1 & g1:
-		x = (x,)
-		yield (m1.difference(x), g1.difference(x), m2.union(x), g2.union(x))
+		x = {x}
+		yield (m1-x, g1-x, m2|x, g2|x)
 		break
 	for x in combinations(m1, 2):
-		yield (m1.difference(x), g1, m2.union(x), g2)
+		x = set(x)
+		yield (m1-x, g1, m2|x, g2)
 	for x in combinations(g1, 2):
-		yield (m1, g1.difference(x), m2, g2.union(x))
+		x = set(x)
+		yield (m1, g1-x, m2, g2|x)
 
 def one_combos(items, next_items):
 	m1, g1 = items
 	m2, g2 = next_items
 
 	for x in m1:
-		x = (x,)
-		yield (m1.difference(x), g1, m2.union(x), g2)
+		x = {x}
+		yield (m1-x, g1, m2|x, g2)
 	for x in g1:
-		x = (x,)
-		yield (m1, g1.difference(x), m2, g2.union(x))
+		x = {x}
+		yield (m1, g1-x, m2, g2|x)
+
+def floor_state(floors):
+	elements = [0] * 8
+	for floor, (mics, gens) in enumerate(floors):
+		for e in mics:
+			elements[e] = floor
+	for mics, gens in floors:
+		count = [0] * 4
+		for e in gens:
+			count[elements[e]] += 1
+		yield sum(n << 3*i for i, n in enumerate(count) if n)
 
 def solve(floors):
+	def process(items):
+		moved = False
+		for m1, g1, m2, g2 in items:
+			if (not (m1 and g1) or m1 <= g1) and (not (m2 and g2) or m2 <= g2):
+				moved = True
+				if floor or m1 or g1:
+					floors[floor] = (m1, g1)
+					floors[next_floor] = (m2, g2)
+					q.append((steps, next_floor, *floors))
+				else:
+					q.append((steps, 0, (m2, g2), *floors[2:]))
+		return moved
+
 	q = deque()
-	q.append((0, 0, floors.copy()))
+	q.append((0, 0, *floors))
 	seen = set()
 
 	while q:
-		steps, floor, floors = q.popleft()
-		state = (floor, *((len(m)<<4) + len(g) for m, g in floors))
+		steps, floor, *floors = q.popleft()
+		state = (floor, *floor_state(floors))
 		if state in seen:
 			continue
 		seen.add(state)
@@ -109,35 +142,24 @@ def solve(floors):
 		if floor < max_floor:
 			next_floor = floor + 1
 			next_items = floors[next_floor]
-			combos = list(filter(valid, two_combos(items, next_items)))
-			if not combos:
-				combos = filter(valid, one_combos(items, next_items))
-			for mics, gens, next_mics, next_gens in combos:
-				floors[floor] = (mics, gens)
-				floors[next_floor] = (next_mics, next_gens)
-				if floor or mics or gens:
-					q.append((steps, next_floor, floors.copy()))
-				else:
-					q.append((steps, 0, floors[1:]))
+			if not process(two_combos(items, next_items)):
+				process(one_combos(items, next_items))
 			floors[next_floor] = next_items
-
 		if floor:
 			next_floor = floor - 1
 			next_items = floors[next_floor]
-			combos = list(filter(valid, one_combos(items, next_items)))
-			if not combos:
-				combos = filter(valid, two_combos(items, next_items))
-			for mics, gens, next_mics, next_gens in combos:
-				floors[floor] = (mics, gens)
-				floors[next_floor] = (next_mics, next_gens)
-				q.append((steps, next_floor, floors.copy()))
+			if not process(one_combos(items, next_items)):
+				process(two_combos(items, next_items))
 	return None
 
 def main():
-	floors = read_input()
+	next_id, floors = read_input()
+	assert next_id <= 6
 	print('Part 1:', solve(floors))
 
-	floors[0] = tuple(items.union(('elerium', 'dilithium')) for items in floors[0])
+	x = {next_id, next_id + 1}
+	m, g = floors[0]
+	floors[0] = (m|x, g|x)
 	print('Part 2:', solve(floors))
 
 # > python3 11.py < data/11.example
