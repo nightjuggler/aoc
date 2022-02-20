277fdc77eb966a1fdc1acc3354c386fb7c6d9951
nightjuggler <pius@nightjuggler.com>
2022-02-20 06:40:32 -0800

    Optimize

--- 2016/16.py
+++ 2016/16.py
@@ -19,16 +19,16 @@ def solve_v2(n, a):
 	assert n > 0 and n % 2 == 0
 	assert a.strip('01') == ''
 
+	pn = len(a)
 	a = [c == '1' for c in a]
-	b = [not c for c in a[::-1]]
+	a.append(False)
+	a.extend(not c for c in a[-2::-1])
 
-	while len(a) < n:
-		c = a.copy()
+	while (an := len(a)) < n:
 		a.append(False)
-		a.extend(b)
-		c.append(True)
-		c.extend(b)
-		b = c
+		a.extend(a[:-1])
+		a[an+1+pn] = True
+		pn = an
 
 	step = (n ^ (n - 1)) // 2 + 1
 	return ''.join(['10'[sum(a[i:i+step]) % 2] for i in range(0, n, step)])
