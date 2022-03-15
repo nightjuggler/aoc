df13e4e72ac8edd8082f955cbe791ffb71929bb5
nightjuggler <pius@nightjuggler.com>
2022-03-14 21:23:59 -0700

    A little more complicated but more precise: When the state of the registers (a, b, c, and d) repeats, the output pattern (0, 1, 0, 1, ...) must also repeat forever.

--- 2016/25-to-c.py
+++ 2016/25-to-c.py
@@ -5,7 +5,7 @@ import sys
 # >>> TIMEFMT="%*E seconds"
 # >>> time (python3 25-to-c.py < data/25.input > 25.c && clang -Ofast 25.c && ./a.out)
 # 182
-# 0.180 seconds
+# 0.185 seconds
 
 # Note: See zshmisc(1) for the zsh time command and zshparam(1) for TIMEFMT
 
@@ -46,11 +46,23 @@ def read_input():
 		elif op == 'out':
 			x, = args
 			assert valid_reg(x) or valid_int(x)
-			code.append('\n\t'.join((
-				f'if ({x} != x) return 0;',
-				'x = !x;',
-				'if (++i == 1000) return 1;'
-				)))
+			code.append(f'if ({x} != x) return 0;' '''
+	x = !x;
+	for (int i = 0; i < num_states; ++i) {
+		int *state = states[i];
+		if (a == state[0] && b == state[1] && c == state[2] && d == state[3])
+			return 1;
+	}
+	if (num_states < 64) {
+		int *state = states[num_states++];
+		state[0] = a;
+		state[1] = b;
+		state[2] = c;
+		state[3] = d;
+	} else {
+		printf("Increase the maximum number of states!\\n");
+		return 1;
+	}''')
 		else:
 			sys.exit(f'Unknown instruction on line {i+1}!')
 
@@ -62,11 +74,13 @@ def main():
 	print('''#include <stdio.h>
 int solve(int a)
 {
-	int i = 0;
 	int x = 0;
 	int b = 0;
 	int c = 0;
 	int d = 0;
+
+	int states[64][4];
+	int num_states = 0;
 ''')
 	for i, line in enumerate(code):
 		if i in jump_targets:
