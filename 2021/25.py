7b88209a7414cb38489dc6329eae233d85ced8bc
nightjuggler <pius@nightjuggler.com>
2022-03-16 22:53:11 -0700

    More compact read_input()

--- 2021/25.py
+++ 2021/25.py
@@ -1,32 +1,14 @@
 import sys
 
 def read_input():
-	row_len = 0
 	grid = []
-
-	for line_number, line in enumerate(sys.stdin, start=1):
-		row = []
-		for c in line.rstrip():
-			if c == '.':
-				row.append(0)
-			elif c == '>':
-				row.append(1)
-			elif c == 'v':
-				row.append(2)
-			else:
-				print(f'Input line {line_number}: Unexpected character!')
-				return None
-		if row_len:
-			if len(row) != row_len:
-				print(f'Input line {line_number}: Unexpected length!')
-				return None
-		else:
-			row_len = len(row)
-			if row_len == 0:
-				print(f'Input line {line_number} is empty!')
-				return None
-		grid.append(row)
-
+	for line_num, line in enumerate(sys.stdin, start=1):
+		try:
+			grid.append(list(map('.>v'.index, line.rstrip())))
+		except ValueError:
+			sys.exit(f'Input line {line_num}: Unexpected character!')
+		if not len(grid[0]) == len(grid[-1]) > 0:
+			sys.exit(f'Input line {line_num}: Unexpected length!')
 	return grid
 
 def move(grid):
