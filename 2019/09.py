b0fa7d7ca4f1145b11c9446f6bdf0abbe042a5ca
nightjuggler <pius@nightjuggler.com>
2022-03-20 12:04:12 -0700

    Add 2019 day 9

---
+++ 2019/09.py
@@ -0,0 +1,80 @@
+import sys
+
+def get_op_modes(number):
+	number, op = divmod(number, 100)
+	modes = []
+	while number:
+		number, mode = divmod(number, 10)
+		assert mode <= 2
+		modes.append(mode)
+	return op, modes
+
+def run(program, inputs):
+	program = program.copy()
+	inputs = iter(inputs)
+	outputs = []
+	i = relative_base = 0
+
+	def get_args(n, store_last=False):
+		nonlocal i
+		if (mlen := len(modes)) < n:
+			modes.extend([0] * (n - mlen))
+		else:
+			assert mlen == n
+		for j, mode in zip(range(1, n+1), modes):
+			arg = program.get(i+j, 0)
+			if mode != 1:
+				if mode == 2:
+					arg += relative_base
+				assert arg >= 0
+				if not (store_last and j == n):
+					arg = program.get(arg, 0)
+			else:
+				assert not (store_last and j == n)
+			yield arg
+		i += n + 1
+
+	def op_add(a, b): return a + b
+	def op_mul(a, b): return a * b
+	def op_lss(a, b): return int(a < b)
+	def op_eql(a, b): return int(a == b)
+
+	def op3(fn):
+		a, b, c = get_args(3, True)
+		program[c] = fn(a, b)
+
+	while True:
+		op, modes = get_op_modes(program.get(i, 0))
+		if   op == 1: op3(op_add)
+		elif op == 2: op3(op_mul)
+		elif op == 3: # -------------------- INPUT
+			a, = get_args(1, True)
+			program[a] = next(inputs)
+		elif op == 4: # -------------------- OUTPUT
+			a, = get_args(1)
+			outputs.append(a)
+		elif op == 5: # -------------------- JUMP-IF-TRUE
+			a, b = get_args(2)
+			if a: i = b
+		elif op == 6: # -------------------- JUMP-IF-FALSE
+			a, b = get_args(2)
+			if not a: i = b
+		elif op == 7: op3(op_lss)
+		elif op == 8: op3(op_eql)
+		elif op == 9: # -------------------- ADJUST-RELATIVE-BASE
+			a, = get_args(1)
+			relative_base += a
+		elif op == 99:
+			break
+		else:
+			sys.exit(f'Unknown opcode {op} at position {i}!')
+	return outputs
+
+def main():
+	program = dict(enumerate(map(int, sys.stdin.readline().split(','))))
+
+	print('Part 1:', run(program, [1]))
+	print('Part 2:', run(program, [2]))
+
+if __name__ == '__main__':
+	main()
