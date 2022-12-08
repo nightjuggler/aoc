2841a73d8ed3b97fcaa462862605e559d2116671
nightjuggler <pius@nightjuggler.com>
2022-12-07 22:02:54 -0800

    Add 2022 day 8

---
+++ 2022/08.py
@@ -0,0 +1,25 @@
+import sys
+def main():
+	rows = [list(map(int, line.strip())) for line in sys.stdin]
+	num_visible = 0
+	max_score = 0
+	for y, row in enumerate(rows):
+		for x, tree in enumerate(row):
+			col = [_row[x] for _row in rows]
+			is_visible = False
+			score = 1
+			for trees in (row[x::-1], row[x:], col[y::-1], col[y:]):
+				distance = 0
+				for height in trees[1:]:
+					distance += 1
+					if height >= tree: break
+				else:
+					is_visible = True
+				score *= distance
+			if is_visible:
+				num_visible += 1
+			if score > max_score:
+				max_score = score
+	print('Part 1:', num_visible)
+	print('Part 2:', max_score)
+main()
