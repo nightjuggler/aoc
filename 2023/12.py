f19a8d7eb82f6cfb7197b7a936bae0044c386797
nightjuggler <pius@nightjuggler.com>
2023-12-12 02:35:30 -0800

    Add 2023 day 12

---
+++ 2023/12.py
@@ -0,0 +1,41 @@
+import sys
+
+seen = {}
+
+def recurse(springs, groups):
+	if not groups:
+		return '#' not in springs
+	state = springs, groups
+	if (result := seen.get(state)) is not None:
+		return result
+	n = len(springs)
+	m = sum(groups) + len(groups) - 1
+	j = groups[0]
+	groups = groups[1:]
+	result = 0
+	for i in range(n - m + 1):
+		if not ('.' in springs[i:j] or j < n and springs[j] == '#'):
+			result += recurse(springs[j+1:], groups)
+		if springs[i] == '#':
+			break
+		j += 1
+	seen[state] = result
+	return result
+
+def part1(data):
+	springs, groups = data
+	return recurse(springs, groups)
+
+def part2(data):
+	springs, groups = data
+	return recurse('?'.join([springs]*5), groups*5)
+
+def tuplify(line):
+	springs, groups = line.split()
+	return springs, tuple(map(int, groups.split(',')))
+
+def main():
+	conditions = list(map(tuplify, sys.stdin))
+	print('Part 1:', sum(map(part1, conditions)))
+	print('Part 2:', sum(map(part2, conditions)))
+main()
