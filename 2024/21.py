87dcc1be389a97612313167f3f064bd8f574e823
nightjuggler <pius@nightjuggler.com>
2024-12-22 06:52:28 -0800

    25% shorter

--- 2024/21.py
+++ 2024/21.py
@@ -1,45 +1,22 @@
 import sys
 
-numpad = {key: (x, y)
-	for y, row in enumerate(('789', '456', '123', '_0A'))
-	for x, key in enumerate(row) if key != '_'}
-dirpad = {key: (x, y)
-	for y, row in enumerate(('_^A', '<v>'))
-	for x, key in enumerate(row) if key != '_'}
-
-def numpad_moves(keys):
-	moves = []
-	x1, y1 = numpad['A']
-	for key in keys:
-		x2, y2 = numpad[key]
-		dx, dy = x2-x1, y2-y1
-		if not x1 and y2 == 3:
-			moves.append('>'*dx)
-			moves.append('v'*dy)
-		elif not x2 and y1 == 3:
-			moves.append('^'*(-dy))
-			moves.append('<'*(-dx))
-		else:
-			if dx < 0: moves.append('<'*(-dx))
-			if dy > 0: moves.append('v'*dy)
-			if dy < 0: moves.append('^'*(-dy))
-			if dx > 0: moves.append('>'*dx)
-		moves.append('A')
-		x1, y1 = x2, y2
-	return ''.join(moves)
-
-def dirpad_lookup():
+def make_lookup(*rows):
+	keypad = {key: (x, y)
+		for y, row in enumerate(rows)
+		for x, key in enumerate(row)}
+	gap = keypad['_']
+	del keypad['_']
 	lookup = {}
-	for key1, (x1, y1) in dirpad.items():
-		for key2, (x2, y2) in dirpad.items():
+	for key1, (x1, y1) in keypad.items():
+		for key2, (x2, y2) in keypad.items():
 			moves = []
 			dx, dy = x2-x1, y2-y1
-			if not x1 and not y2:
-				moves.append('>'*dx)
-				moves.append('^'*(-dy))
-			elif not x2 and not y1:
-				moves.append('v'*dy)
-				moves.append('<'*(-dx))
+			if (x1, y2) == gap:
+				moves.append('>'*dx if dx > 0 else '<'*(-dx))
+				moves.append('v'*dy if dy > 0 else '^'*(-dy))
+			elif (x2, y1) == gap:
+				moves.append('v'*dy if dy > 0 else '^'*(-dy))
+				moves.append('>'*dx if dx > 0 else '<'*(-dx))
 			else:
 				if dx < 0: moves.append('<'*(-dx))
 				if dy > 0: moves.append('v'*dy)
@@ -51,19 +28,21 @@ def dirpad_lookup():
 
 def main():
 	cache = {}
-	lookup = dirpad_lookup()
+	numpad = make_lookup('789', '456', '123', '_0A')
+	dirpad = make_lookup('_^A', '<v>')
+
+	def numpad_moves(code):
+		key1 = 'A'
+		return ''.join([numpad[key1, key1 := key2] for key2 in code])
 
 	def dirpad_moves(keys, depth):
 		if not depth:
 			return len(keys)
 		if (keys, depth) in cache:
 			return cache[keys, depth]
-		n = 0
 		key1 = 'A'
-		for key2 in keys:
-			n += dirpad_moves(lookup[key1, key2], depth-1)
-			key1 = key2
-		cache[keys, depth] = n
+		cache[keys, depth] = n = sum(
+			dirpad_moves(dirpad[key1, key1 := key2], depth-1) for key2 in keys)
 		return n
 
 	sum1 = sum2 = 0
@@ -71,10 +50,9 @@ def main():
 		assert line.endswith('A\n')
 		code = int(line[:-2])
 		keys = numpad_moves(line[:-1])
-		n1 = dirpad_moves(keys, 2)
-		n2 = dirpad_moves(keys, 25)
-		sum1 += code * n1
-		sum2 += code * n2
+		sum1 += code * dirpad_moves(keys, 2)
+		sum2 += code * dirpad_moves(keys, 25)
+
 	print('Part 1:', sum1)
 	print('Part 2:', sum2)
 main()
