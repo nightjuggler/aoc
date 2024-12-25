9f38cb6b8f71713efac6499db9058b7be98d73bb
nightjuggler <pius@nightjuggler.com>
2024-12-24 23:22:35 -0800

    Clean up

--- 2024/24.py
+++ 2024/24.py
@@ -1,7 +1,10 @@
-import argparse
 from collections import defaultdict, deque
 import re
 import sys
+
+class PuzzleError(Exception): pass
+
+def err(message): raise PuzzleError(message)
 
 def op_and(a, b): return a & b
 def op_or(a, b): return a | b
@@ -12,16 +15,19 @@ def read_input(f):
 	pattern1 = re.compile(f'^{w}: (0|1)$')
 	pattern2 = re.compile(f'^{w} (AND|OR|XOR) {w} -> {w}$')
 	ready = {}
+	line_num = 0
 	for line_num, line in enumerate(f, start=1):
 		if line == '\n': break
 		if not (m := pattern1.match(line)):
-			sys.exit(f'Line {line_num} doesn\'t match pattern!')
+			err(f'Line {line_num} doesn\'t match pattern!')
 		wire, value = m.groups()
 		ready[wire] = int(value)
 	wires = defaultdict(list)
+	if not line_num:
+		return ready, wires
 	for line_num, line in enumerate(f, start=line_num+1):
 		if not (m := pattern2.match(line)):
-			sys.exit(f'Line {line_num} doesn\'t match pattern!')
+			err(f'Line {line_num} doesn\'t match pattern!')
 		in1, op, in2, out = m.groups()
 		wires[in1].append([in2, op, out])
 		wires[in2].append([in1, op, out])
@@ -29,7 +35,10 @@ def read_input(f):
 
 def numbered_wires(wires, x):
 	wires = sorted(wire for wire in wires if wire[0] == x)
-	assert all(wire == f'{x}{i:02}' for i, wire in enumerate(wires))
+	if not all(wire == f'{x}{i:02}' for i, wire in enumerate(wires)):
+		err(f'The wires beginning with {x} are not numbered correctly!')
+	if not wires:
+		err(f'There are no wires beginning with {x}!')
 	return wires
 
 def part1(ready, wires):
@@ -49,7 +58,8 @@ def part1(ready, wires):
 			if wire2 in ready:
 				ready[wire3] = ops[op](value1, ready[wire2])
 				q.append(wire3)
-	assert set(ready) == all_wires
+	if set(ready) != all_wires:
+		err('Not all wires have a value!')
 	zs = numbered_wires(ready, 'z')
 	return sum(1<<i for i, wire in enumerate(zs) if ready[wire])
 
@@ -68,57 +78,63 @@ def part2(wires):
 		del lookup[out2]
 		wrong.add(out1)
 		wrong.add(out2)
+		return out2
 
 	def add(wire, check_z=False):
-		if len(wires[wire]) != 2:
-			sys.exit(f'Expected {wire} to be the input to exactly two gates!')
+		def fail():
+			err(f'Expected {wire} to be the input to one XOR and one AND gate with {wire2}!')
+		wire2 = 'y' + wire[1:] if wire[0] == 'x' else overflow[2]
+		if (len(wires[wire]) != 2 and (wire[0] == 'x' or not wires[wire2] or
+			len(wires[wire := fix(wire, wires[wire2][0][0])]) != 2)): fail()
 		g1, g2 = wires[wire]
-		wire2 = 'y' + wire[1:] if wire[0] == 'x' else overflow1[2]
-		if not g1[0] == g2[0] == wire2:
-			sys.exit(f'Expected {wire} to be the input to two gates with {wire2}!')
+		if not g1[0] == g2[0] == wire2: fail()
 		if (g1[1], g2[1]) != ('XOR', 'AND'):
 			g1, g2 = g2, g1
-			if (g1[1], g2[1]) != ('XOR', 'AND'):
-				sys.exit(f'Expected {wire} to be the input to an XOR and an AND gate!')
+			if (g1[1], g2[1]) != ('XOR', 'AND'): fail()
 		if check_z:
 			z = 'z' + check_z[1:]
 			if g1[2] != z: fix(g1[2], z)
 			return g2
 		return g1, g2
 
+	def carry(wire):
+		def fail():
+			err(f'Expected {wire} to be the input to one OR gate with {wire2}!')
+		wire2 = overflow[2]
+		if (len(wires[wire]) != 1 and (not wires[wire2] or
+			len(wires[wire := fix(wire, wires[wire2][0][0])]) != 1)): fail()
+		g1, = wires[wire]
+		if g1[0] != wire2 or g1[1] != 'OR': fail()
+		return g1
+
 	xs = numbered_wires(wires, 'x')
 	x = xs.pop(0)
-	overflow1 = add(x, x)
+	overflow = add(x, x)
 
 	for x in xs:
 		xor, overflow2 = add(x)
-		if len(wires[xor[2]]) != 2:
-			if wires[overflow1[2]]:
-				fix(xor[2], wires[overflow1[2]][0][0])
-
-		overflow1 = add(xor[2], x)
-		if len(wires[overflow2[2]]) != 1:
-			if wires[overflow1[2]]:
-				fix(overflow2[2], wires[overflow1[2]][0][0])
-			if len(wires[overflow2[2]]) != 1:
-				sys.exit(f'Expected {overflow2[2]} to be the input to exactly one gate!')
-
-		if wires[overflow2[2]][0][:2] != [overflow1[2], 'OR']:
-			sys.exit(f'Expected {overflow2[2]} to be the input to an OR gate with {overflow1[2]}!')
-		overflow1 = wires[overflow2[2]][0]
+		overflow = add(xor[2], x)
+		overflow = carry(overflow2[2])
 
 	z = f'z{len(xs)+1:02}'
-	if overflow1[2] != z: fix(overflow1[2], z)
+	if overflow[2] != z: fix(overflow[2], z)
 
 	return ','.join(sorted(wrong))
 
 def main():
-	parser = argparse.ArgumentParser(allow_abbrev=False)
-	parser.add_argument('--part2', action='store_true')
-	args = parser.parse_args()
-
-	ready, wires = read_input(sys.stdin)
-	print('Part 1:', part1(ready, wires))
-	if args.part2:
-		print('Part 2:', part2(wires))
+	try:
+		ready, wires = read_input(sys.stdin)
+	except PuzzleError as e:
+		print(e)
+		return
+	try:
+		result = part1(ready, wires)
+	except PuzzleError as e:
+		result = str(e)
+	print('Part 1:', result)
+	try:
+		result = part2(wires)
+	except PuzzleError as e:
+		result = str(e)
+	print('Part 2:', result)
 main()
