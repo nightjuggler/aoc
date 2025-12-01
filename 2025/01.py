87d685b8656489c83fc31cd7b44ae51423d63cb8
nightjuggler <pius@nightjuggler.com>
2025-11-30 23:50:02 -0800

    Add 2025 day 1

---
+++ 2025/01.py
@@ -0,0 +1,38 @@
+import re
+import sys
+
+def part1(rots):
+	dial = 50
+	return sum(1 for n in rots if not (dial := (dial + n) % 100))
+
+def part2(rots):
+	dial = 50
+	password = 0
+	for n in rots:
+		if n < 0:
+			d, n = divmod(-n, 100)
+			password += d + (n >= dial > 0)
+			dial -= n
+			dial %= 100
+		else:
+			dial += n
+			if dial > 99:
+				password += dial // 100
+				dial %= 100
+	return password
+
+def main(f):
+	pattern = re.compile('^[LR][1-9][0-9]*$')
+	rots = []
+	for line_num, line in enumerate(f, start=1):
+		if not pattern.match(line):
+			return f'Line {line_num} doesn\'t match the expected pattern!'
+		n = int(line[1:])
+		if line[0] == 'L': n = -n
+		rots.append(n)
+
+	print('Part 1:', part1(rots))
+	print('Part 2:', part2(rots))
+
+if __name__ == '__main__':
+	sys.exit(main(sys.stdin))
