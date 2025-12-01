7b013e243466e488121aba98045ece7af4de65a9
nightjuggler <pius@nightjuggler.com>
2025-12-01 13:41:18 -0800

    Simplify

--- 2025/01.py
+++ 2025/01.py
@@ -1,37 +1,27 @@
 import re
 import sys
 
-def part1(rots):
-	dial = 50
-	return sum(1 for n in rots if not (dial := (dial + n) % 100))
-
-def part2(rots):
-	dial = 50
-	password = 0
-	flipped = False
-	for n in rots:
-		if (left := n < 0): n = -n
-		if left != flipped:
-			if dial: dial = 100 - dial
-			flipped = left
-		dial += n
-		if dial > 99:
-			password += dial // 100
-			dial %= 100
-	return password
-
 def main(f):
+	dial = 50
+	flipped = False
+	part1 = part2 = 0
 	pattern = re.compile('^[LR][1-9][0-9]*$')
-	rots = []
+
 	for line_num, line in enumerate(f, start=1):
 		if not pattern.match(line):
 			return f'Line {line_num} doesn\'t match the expected pattern!'
-		n = int(line[1:])
-		if line[0] == 'L': n = -n
-		rots.append(n)
+		left = line[0] == 'L'
+		if flipped != left:
+			if dial: dial = 100 - dial
+			flipped = left
+		dial += int(line[1:])
+		if dial > 99:
+			part2 += dial // 100
+			dial %= 100
+			if not dial: part1 += 1
 
-	print('Part 1:', part1(rots))
-	print('Part 2:', part2(rots))
+	print('Part 1:', part1)
+	print('Part 2:', part2)
 
 if __name__ == '__main__':
 	sys.exit(main(sys.stdin))
