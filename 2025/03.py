dc7ea319b9558b2d222bc1836a2cec17ac6c37a9
nightjuggler <pius@nightjuggler.com>
2025-12-03 02:00:44 -0800

    Tweak

--- 2025/03.py
+++ 2025/03.py
@@ -1,12 +1,10 @@
 import sys
 
-def solve(jolts, seqlen):
-	num_batteries = sum(map(len, jolts))
-	jolts = list(enumerate(jolts))[::-1]
-	max_joltage = 0
+def solve(jolts, maxlen, seqlen):
+	max_jolts = 0
 	min_index = -1
-	for max_index in range(num_batteries - seqlen, num_batteries):
-		max_joltage *= 10
+	for max_index in range(maxlen - seqlen, maxlen):
+		max_jolts *= 10
 		for j, indices in jolts:
 			for i in indices:
 				if i > min_index: break
@@ -14,18 +12,21 @@ def solve(jolts, seqlen):
 				continue
 			if i <= max_index:
 				min_index = i
-				max_joltage += j
+				max_jolts += j
 				break
-	return max_joltage
+	return max_jolts
 
 def main():
 	total1 = total2 = 0
 	for line in sys.stdin:
+		line = line.strip()
+		size = len(line)
 		jolts = [[] for j in range(10)]
-		for i, j in enumerate(map(int, line.strip())):
+		for i, j in enumerate(map(int, line)):
 			jolts[j].append(i)
-		total1 += solve(jolts, 2)
-		total2 += solve(jolts, 12)
+		jolts = list(enumerate(jolts))[::-1]
+		total1 += solve(jolts, size, 2)
+		total2 += solve(jolts, size, 12)
 	print('Part 1:', total1)
 	print('Part 2:', total2)
 
