2674db1f3497137df3a2c3ef76a67a1e0818bc6d
nightjuggler <pius@nightjuggler.com>
2025-12-05 02:28:37 -0800

    Simplify

--- 2025/05.py
+++ 2025/05.py
@@ -10,14 +10,12 @@ def main(lines):
 
 	print('Part 1:', sum(any(a <= n < b for a, b in ranges) for n in map(int, lines)))
 
-	ids = sorted(set().union(*ranges))
-	fresh = [False] * (len(ids) - 1)
-	for a, b in ranges:
-		a = ids.index(a)
-		b = ids.index(b)
-		fresh[a:b] = [True] * (b - a)
-
-	print('Part 2:', sum(ids[i+1] - ids[i] for i, good in enumerate(fresh) if good))
+	fresh = prev_b = 0
+	for a, b in sorted(ranges):
+		if b > prev_b:
+			fresh += b - (prev_b if prev_b > a else a)
+			prev_b = b
+	print('Part 2:', fresh)
 
 if __name__ == '__main__':
 	main(sys.stdin)
